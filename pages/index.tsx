8cfcf5b75aeb49a3d1cdf5db67c5c6fb95f8b79e
Felipe Urbanski <felipeurbansk@gmail.com>
2020-08-26 22:37:31 -0300

    Initial commit, config typescript and eslint

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className=""></main>
+
+      <footer className="">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      </footer>
+    </div>
+  )
+}
